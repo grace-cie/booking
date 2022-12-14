6ee7c805d22cfc7b18b64b1e10cc7b227b72429e
grace-cie <rirei1415@gmail.com>
2022-12-14 14:43:13 +0800

    added controller for doctor
    able to fetch doctors

---
+++ backend/app/Http/Controllers/DocController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+// namespace App\Http\AuthController;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class DocController extends AuthController {
+
+     public function __construct(){
+          $this->middleware('auth:api', ['except' => [
+               'getDoctors'
+          ]]);
+     }
+     public function getDoctors(){
+          $doctors = DB::select("select * from users where user_class='doctor' limit 5");
+          $res = response()->json($doctors);
+          return $res;
+     }
+     
+}
